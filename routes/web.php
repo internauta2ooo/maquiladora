349b6545ec6094b30b53b33319faf3a4c45db995
Eduardo <eduardo>
2020-08-22 17:13:29 -0500

    auth

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 Route::view("/marca","marca");
 Route::get('/run', function () {
     return view('welcome');
