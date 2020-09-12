d0014bdf9e6c6858f54ee065b97812138a8a3132
Eduardo <eduardo>
2020-09-12 11:08:50 -0500

    agregando detalles

--- resources/js/components/OrdenesEntrega.vue
+++ resources/js/components/OrdenesEntrega.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="container">
-  <p>try and catch</p>
     <div>
-      <b-table striped hover :items="items"></b-table>
+      <b-table :items="items" :fields="fields" striped responsive="sm">
+        <template v-slot:cell(show_details)="row">
+          <b-button
+            size="sm"
+            @click="row.toggleDetails"
+            class="mr-2"
+          >{{ row.detailsShowing ? 'Esconder' : 'Mostrar'}} Detalles</b-button>
+          <!-- As `row.showDetails` is one-way, we call the toggleDetails function on @change -->
+        </template>
+        <template v-slot:row-details="row">
+          <b-card>
+            <b-row class="mb-2">
+              <b-col sm="3" class="text-sm-right">
+                <b>Coordinado</b>
+              </b-col>
+              <b-col>{{ row.item.numerodeprendas }}</b-col>
+            </b-row>
+            <b-button size="sm" @click="row.toggleDetails">Esconder detalles</b-button>
+          </b-card>
+        </template>
+      </b-table>
     </div>
   </div>
 </template>
@@ -11,11 +30,60 @@
 export default {
   data() {
     return {
+      fields: [
+        { key: "id_orden", label: "Numero de Orden" },
+        { key: "marca", label: "marca" },
+        { key: "fecha_entrega", label: "Fecha de Entrega" },
+        "show_details",
+      ],
       items: [
-        { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
-        { age: 21, first_name: "Larsen", last_name: "Shaw" },
-        { age: 89, first_name: "Geneva", last_name: "Wilson" },
-        { age: 38, first_name: "Jami", last_name: "Carney" },
+        {
+          isActive: true,
+          id_orden: 10,
+          marca: "Polo",
+          fecha_entrega: "12/12/2021",
+          numerodeprendas: [
+            {
+              id_entrada: 1,
+              color: "rojo",
+              tallas: [
+                { descripcion: "mediana", numero: 15 },
+                { descripcion: "xl", numero: 1 },
+                { descripcion: "chica", numero: 150 },
+              ],
+            },
+          ],
+        },
+        {
+          isActive: true,
+          id_orden: 40,
+          marca: "DC",
+          fecha_entrega: "12/12/2021",
+          numerodeprendas: [
+            { talla: "Mediana", numero: 15 },
+            { talla: "XL", numero: 25 },
+          ],
+        },
+        {
+          isActive: true,
+          id_orden: 30,
+          marca: "Dickerson",
+          fecha_entrega: "12/12/2021",
+          numerodeprendas: [
+            { talla: "Mediana", numero: 15 },
+            { talla: "XL", numero: 25 },
+          ],
+        },
+        {
+          isActive: true,
+          id_orden: 20,
+          marca: "Alexis",
+          fecha_entrega: "12/12/2021",
+          numerodeprendas: [
+            { talla: "Mediana", numero: 15 },
+            { talla: "XL", numero: 25 },
+          ],
+        },
       ],
     };
   },
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,26 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-
-            </div>
-            <div id="app" class="content">
-                <!--La equita id debe ser app, como hemos visto en app.js-->
-                <example-componentt></example-componentt>
-                <ordenes-entrega></ordenes-entrega>
+        <div class="col-md-12">
+            <div id="app" class="content">            
                 <!--Añadimos nuestro componente vuejs-->
+                <ordenes-entrega></ordenes-entrega>
+                
             </div>
         </div>
     </div>
