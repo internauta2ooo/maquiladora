5b4b32482109d66315af2103a524537e52a91a9b
Eduardo <eduardo>
2020-09-12 15:17:13 -0500

    se agrega total y detalles de la entrada de orden

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,7 +23,7 @@ Vue.use(BootstrapVue)
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-componentt', require('./components/ExampleComponentt.vue').default);
+
 Vue.component('ordenes-entrega', require('./components/OrdenesEntrega.vue').default);
 
 /**
--- resources/js/components/OrdenesEntrega.vue
+++ resources/js/components/OrdenesEntrega.vue
@@ -14,27 +14,52 @@
           <b-card>
             <b-row class="mb-2">
               <b-col sm="3" class="text-sm-right">
-                <b>Coordinado</b>
+                <b>Entradas de la orden</b>
               </b-col>
-              <b-col>{{ row.item.numerodeprendas }}</b-col>
+
+              <ul id="listaordenes" style="list-style-type:none;">
+                <li id="ordenes" v-for="item in row.item.numerodeprendas">
+                  <strong>Id entrada:</strong>
+                  {{ item.id_entrada }}
+                  <strong>&nbsp;&nbsp;Coordinado:</strong>
+                  {{ item.coordinado }}
+                  <strong>&nbsp;&nbsp;Color:</strong>
+                  {{ item.color }}
+                  <strong>&nbsp;&nbsp;Talla:</strong>
+                  {{ item.descripcion }}
+                  <strong>&nbsp;&nbsp;Piezas:</strong>
+                  {{ item.numero }}
+                </li>
+              </ul>
+              <b-col sm="3" class="text-sm-right">
+                <b>Total de piezas: </b>{{row.item.totalpiezas}}
+              </b-col>
             </b-row>
             <b-button size="sm" @click="row.toggleDetails">Esconder detalles</b-button>
           </b-card>
         </template>
       </b-table>
     </div>
+    <b-button variant="primary" @click="actualizarAlgo()">Ya soy reactivo</b-button>
   </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    actualizarAlgo() {
+      console.log("run it");
+      console.log(this.items[0]);
+      this.items[0].marca = "Reactivo";
+    },
+  },
   data() {
     return {
       fields: [
         { key: "id_orden", label: "Numero de Orden" },
-        { key: "marca", label: "marca" },
+        { key: "marca", label: "Marca" },
         { key: "fecha_entrega", label: "Fecha de Entrega" },
-        "show_details",
+        { key: "show_details", label: "Mostrar Detalles" },
       ],
       items: [
         {
@@ -42,15 +67,28 @@ export default {
           id_orden: 10,
           marca: "Polo",
           fecha_entrega: "12/12/2021",
+          totalpiezas:100,
           numerodeprendas: [
             {
               id_entrada: 1,
+              coordinado: "nike color",
               color: "rojo",
-              tallas: [
-                { descripcion: "mediana", numero: 15 },
-                { descripcion: "xl", numero: 1 },
-                { descripcion: "chica", numero: 150 },
-              ],
+              descripcion: "mediana",
+              numero: 15,
+            },
+            {
+              id_entrada: 1,
+              coordinado: "nike dos.0",
+              color: "azul",
+              descripcion: "mediana",
+              numero: 25,
+            },
+            {
+              id_entrada: 1,
+              coordinado: "stud",
+              color: "verde",
+              descripcion: "grande",
+              numero: 35,
             },
           ],
         },
@@ -59,9 +97,22 @@ export default {
           id_orden: 40,
           marca: "DC",
           fecha_entrega: "12/12/2021",
+          totalpiezas:200,
           numerodeprendas: [
-            { talla: "Mediana", numero: 15 },
-            { talla: "XL", numero: 25 },
+            {
+              id_entrada: 1,
+              coordinado: "masal",
+              color: "rojo",
+              descripcion: "xl",
+              numero: 45,
+            },
+            {
+              id_entrada: 1,
+              coordinado: "nikecolor3000",
+              color: "rojo",
+              descripcion: "mxl",
+              numero: 105,
+            },
           ],
         },
         {
@@ -69,9 +120,29 @@ export default {
           id_orden: 30,
           marca: "Dickerson",
           fecha_entrega: "12/12/2021",
+          totalpiezas:250,
           numerodeprendas: [
-            { talla: "Mediana", numero: 15 },
-            { talla: "XL", numero: 25 },
+            {
+              id_entrada: 1,
+              coordinado: "tsuto",
+              color: "rojo verdoso",
+              descripcion: "XL",
+              numero: 15,
+            },
+            {
+              id_entrada: 1,
+              coordinado: "noo",
+              color: "azul",
+              descripcion: "SL",
+              numero: 1005,
+            },
+            {
+              id_entrada: 1,
+              coordinado: "nike zer",
+              color: "rojo",
+              descripcion: "mediana",
+              numero: 15,
+            },
           ],
         },
         {
@@ -79,9 +150,15 @@ export default {
           id_orden: 20,
           marca: "Alexis",
           fecha_entrega: "12/12/2021",
+          totalpiezas:1010,
           numerodeprendas: [
-            { talla: "Mediana", numero: 15 },
-            { talla: "XL", numero: 25 },
+            {
+              id_entrada: 1,
+              coordinado: "basket",
+              color: "blanco",
+              descripcion: "SL",
+              numero: 159,
+            },
           ],
         },
       ],
@@ -92,3 +169,10 @@ export default {
   },
 };
 </script>
+<style scoped>
+#ordenes{
+  border-style: solid;
+  border-color: rgb(199, 199, 199);
+  margin: 5px;
+}
+</style>
