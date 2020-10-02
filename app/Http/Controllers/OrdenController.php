bbcf251c5dde6a17389fe747099ed9491b3464b0
Eduardo <eduardo>
2020-10-02 11:18:06 -0500

    se inserta en la base de datos las ordenes de entrega

--- app/Http/Controllers/OrdenController.php
+++ app/Http/Controllers/OrdenController.php
@@ -23,11 +23,41 @@ class OrdenController extends Controller
         if ($request->nuevaMarca) {
             $idMarcaNueva = $objOrdenServicio->insertarMarca($request->nombreMarca, $request->nombreMarca);
         }
-        //$marcaId, $folio, $modelo, $prenda, $coordinado
-        $objOrdenServicio->insertarOrdenMaquila($idMarcaNueva, $request->numeroOrden, $request->modelo, $request->prenda, $request->coordinado);
-
+        $ordenEntrega = $objOrdenServicio->insertarOrdenMaquila(
+            $idMarcaNueva,
+            $request->numeroOrden,
+            $request->modelo,
+            $request->prenda,
+            $request->coordinado
+        );
+        $filasTotalEntradas = $request->totalEntradas;
+        foreach ($filasTotalEntradas as $item) {
+            $cantidadOrdenesId = $objOrdenServicio->insertarCantidadOrdenes($ordenEntrega);
+            var_dump("insertando un arreglo...");
+            var_dump($item);
+            $itemKeys = array_keys($item);
+            var_dump(array_keys($item));
+            foreach ($item as $key => $subItem) {
+                var_dump("insertando las tallas...");
+                var_dump($subItem);
+                var_dump("y la key..." . $key);
+                $coordinadoId = $item["coordinado"];
+                $colorId = $item["color"];
+                if ($key != "coordinado" && $key != "color") {
+                    $objOrdenServicio->insertarCantidadOrdenesTallas(
+                        $cantidadOrdenesId,
+                        $coordinadoId,
+                        $colorId,
+                        $key,
+                        $subItem,
+                        "0"
+                    );
+                }
+            }
+        }
         return $request;
     }
+
     public function crearPdfOrdenTrabajo()
     {
         OrdenServicios::crearPdfOrden();
--- app/Http/Servicios/OrdenServicios.php
+++ app/Http/Servicios/OrdenServicios.php
@@ -14,11 +14,38 @@ class OrdenServicios
 
     static public function insertarOrdenMaquila($marcaId, $folio, $modelo, $prenda, $coordinado)
     {
-        $id = DB::table('orden_entrega')->insertGetId(["marca_id" => $marcaId, "folio_id" => $folio, "modelo_id" => $modelo, "prenda_id" => $prenda, "coordinado_id" => $coordinado]);
-        var_dump("orden entrega");
+        $id = DB::table('orden_entrega')->insertGetId([
+            "marca_id" => $marcaId, "folio_id" => $folio, "modelo_id" => $modelo,
+            "prenda_id" => $prenda, "coordinado_id" => $coordinado
+        ]);
+        var_dump("orden entrega 3.0");
         var_dump($id);
         return $id;
     }
+
+    static public function insertarCantidadOrdenes($ordenEntregaId)
+    {
+        $id = DB::table('cantidad_ordenes')->insertGetId(["orden_entrega_id" => $ordenEntregaId]);
+        var_dump("cantidad ordenes");
+        var_dump($id);
+        return $id;
+    }
+
+    static public function insertarCantidadOrdenesTallas($cantidadOrdenesId, $coordinadoId, $colorId, $tallaId, $cantidadOrden, $cantidadOrdenEntregas)
+    {
+        $id = DB::table('cantidad_ordenes_tallas')->insertGetId([
+            "cantidad_ordenes_id" => $cantidadOrdenesId,
+            "coordinado_id" => $coordinadoId,
+            "color_id" => $colorId,
+            "talla_id" => $tallaId,
+            "cantidad_orden" => $cantidadOrden,
+            "cantidad_orden_entregadas" => $cantidadOrdenEntregas
+        ]);
+        var_dump("cantidad ordenes tallas");
+        var_dump($id);
+        return $id;
+    }
+
     static public function insertarMarca($nombre, $descripcion)
     {
         $id = DB::table('marca')->insertGetId(["nombre" => $nombre, "descripcion" => $descripcion]);
@@ -26,6 +53,7 @@ class OrdenServicios
         var_dump($id);
         return $id;
     }
+
     static public function insertaCantidadOrdenes($ordenEntregaId)
     {
         $id = DB::table('cantidad_ordenes')->insertGetId(["orden_entrega_id" => $ordenEntregaId]);
