a4878304be2dd3c123c4924af749ec22de8ee7fd
Eduardo <eduardo>
2020-11-16 22:14:19 -0600

    cambios pdf

--- resources/views/marcapdf.blade.php
+++ resources/views/marcapdf.blade.php
@@ -5,7 +5,7 @@
     <title></title>
   </head>
   <body>
-    <table>
+    <table class="banner">
     <tbody>
       <tr>
         <td class="td-25">
@@ -25,50 +25,84 @@
         </td>
 
       </tr>
+      <tr>
+        <td class="td-25">
+<p>Fecha entrega: {{$filasOrdenadas["datos_orden"]["fecha_entrega"]}}</p>
+        </td>
+        <td class="td-25">
+          <p>Usuario: {{$filasOrdenadas["datos_orden"]["usuario"]}}</p>
+        </td>
+        <td class="td-25">
+          <p>Total prendas:  {{$filasOrdenadas["datos_orden"]["total_piezas"]}}</p>
+        </td>
+       
+
+
+      </tr>
+      <tr>
+        <td class="td-25">
+          <p>Muestra original:  {{$filasOrdenadas["datos_orden"]["muestra_original"]}}</p>
+        </td>
+        <td class="td-25">
+          <p>Muestra referencia:  {{$filasOrdenadas["datos_orden"]["muestra_referencia"]}}</p>
+        </td>
+        <td class="td-25">
+          <p>Prendas:  {{$filasOrdenadas["datos_orden"]["prenda"]}}</p>
+        </td>
+      </tr>
     </tbody>
   </table>
    
   
 
 
-<p>Fecha entrega: {{$filasOrdenadas["datos_orden"]["fecha_entrega"]}}</p>
-<p>Usuario: {{$filasOrdenadas["datos_orden"]["usuario"]}}</p>
-<p>Total prendas:  {{$filasOrdenadas["datos_orden"]["total_piezas"]}}</p>
 
-<p>Prendas:  {{$filasOrdenadas["datos_orden"]["prenda"]}}</p>
-<p>Muestra original:  {{$filasOrdenadas["datos_orden"]["muestra_original"]}}</p>
-<p>Muestra referencia:  {{$filasOrdenadas["datos_orden"]["muestra_referencia"]}}</p>
+
+
+
+
+
+
   
   
  
  
   
   
-  
-    <table class="table table-bordered">               
+  <div id="tabla">
+   
+    <table id="tablatallas" cellspacing="0" cellpadding="0" align="center" border="1">               
         @foreach($filasOrdenadas["lista"] as $filas)
-      <tr>
+      <tr class="trclass">
         @foreach($filas as $columnas)
-        <td>{{$columnas}}</td>       
+        <td class="tdclass">{{$columnas}}</td>       
           @endforeach   
       </tr>
         @endforeach            
     </table>
+    
+    </div>
   </body>
 </html>
 <style>
 
-  table, th, td {
-  border: 1px solid black;
+.trclass,.tdclass{
+padding:10px;
+margin:10px;
 }
 .td-25{
     width: 50%;
-    padding:10px;
+    padding:0px;
 }
 .td-50{
     width: 50%;
 }
 .td-100{
     width: 100%;
+}
+#tabla{
+ padding-top:15px;
+ margin-top:15px;
+
 }
 </style>
