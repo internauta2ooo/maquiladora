7f47cc53a522c3a7dc1fb266ae6fa64a51fc4cf8
Eduardo <61359082+internauta2ooo@users.noreply.github.com>
2021-05-05 13:34:56 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes00
+| Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
